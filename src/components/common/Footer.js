abd7d0aba17141673589444ec823b53374260ed7
u_zo <kwoojo0127@naver.com>
2020-06-14 02:05:19 +0900

    Add header, footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterBlock = styled.div`
+  height: 5rem;
+`;
 
 const Footer = () => {
     return (
         <>
+            <FooterBlock/>
         </>
     );
 };
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
 import styled from 'styled-components';
+import Responsive from './Responsive';
 import {Link} from 'react-router-dom';
 
-const HeaderBlock = styled(Row)`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto');
-  font-family: 'Roboto', sans-serif;
+const HeaderBlock = styled.div`
   position: fixed;
   width: 100%;
   background: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
 `;
 
-const Wrapper = styled(Col)`
+const Wrapper = styled.div`
+  padding-left: 4rem;
+  padding-right: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
   
+  .logo {
+    font-size: 1.125rem;
+    letter-spacing: 2px;
+    padding: 0 2rem;
+    text-decoration: none;
+  }
+  
+  @media (max-width: 768px) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 `;
 
 const Header = () => {
     return (
-        <Container fluid={true}>
+        <>
             <HeaderBlock>
-                <Col>
-                    <Link to="/">Woojo</Link>
-                </Col>
+                <Wrapper>
+                    <Link to="/" class="logo">U-Zo</Link>
+                </Wrapper>
             </HeaderBlock>
-        </Container>
+        </>
     );
 };
 
