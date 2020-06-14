3720b5ea0df8f4dbbea4d960286f9326e805408a
u_zo <kwoojo0127@naver.com>
2020-06-14 15:00:49 +0900

    Use main page for section contents

---
+++ src/pages/MainPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/common/Header';
+import Top from '../components/section/Top';
+import Footer from '../components/common/Footer';
+import About from '../components/section/About';
+import Skills from '../components/section/Skills';
+import Projects from '../components/section/Projects';
+import Contact from '../components/section/Contact';
+
+const MainPage = () => {
+    return (
+        <>
+            <Header/>
+            <Top/>
+            <About/>
+            <Skills/>
+            <Projects/>
+            <Contact/>
+            <Footer/>
+        </>
+    );
+}
+
+export default MainPage;
