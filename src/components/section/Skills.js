f7653c460d80b49494336a4ef772aba0b02d1cd1
u_zo <kwoojo0127@naver.com>
2020-06-15 23:09:16 +0900

    Add card layout for skill list

--- src/components/section/Skills.js
+++ src/components/section/Skills.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import Responsive from '../common/Responsive';
+
+const CardBlock = styled.div`
+  width: 100%;
+`;
+
+const SkillsBlock = styled.div`
+  background: #fafafa;
+  padding-top: 8rem;
+  padding-bottom: 8rem;
+`;
+
+const Wrapper = styled(Responsive)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Card = ({list}) => {
+    return (
+        <CardBlock>
+
+        </CardBlock>
+    );
+};
 
 const Skills = () => {
     return (
-        <>
-        </>
+        <SkillsBlock>
+            <Wrapper>
+                <Card list={}/>
+            </Wrapper>
+        </SkillsBlock>
     );
-}
+};
 
 export default Skills;
