46a685061281982a7ccd4f735b7a100731aa253d
u_zo <kwoojo0127@naver.com>
2020-06-17 15:37:42 +0900

    Update card component for skill list

--- src/components/section/Skills.js
+++ src/components/section/Skills.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
+import {DiHtml5, DiCss3, DiJsBadge} from 'react-icons/di';
 
 const CardBlock = styled.div`
-  width: 100%;
+  text-align: center;
+  width: 30%;
+  
+  .skill {
+    font-size: 1.1rem;
+    padding-bottom: 10px;
+  }
 `;
 
 const SkillsBlock = styled.div`
   background: #fafafa;
-  padding-top: 8rem;
-  padding-bottom: 8rem;
+  padding-top: 4rem;
+  padding-bottom: 4rem;
 `;
 
 const Wrapper = styled(Responsive)`
@@ -18,10 +25,16 @@ const Wrapper = styled(Responsive)`
   align-items: center;
 `;
 
-const Card = ({list}) => {
+const Card = ({icon, skill, text}) => {
     return (
         <CardBlock>
-
+            {icon}
+            <p className="skill">
+                {skill}
+            </p>
+            <p>
+                {text}
+            </p>
         </CardBlock>
     );
 };
@@ -29,8 +42,11 @@ const Card = ({list}) => {
 const Skills = () => {
     return (
         <SkillsBlock>
+            Skills
             <Wrapper>
-                <Card list={}/>
+                <Card icon={<DiHtml5 size="125" color="#FF8200"/>} skill="HTML" text="안녕"/>
+                <Card icon={<DiCss3 size="125" color="#646EFF"/>} skill="CSS" text="안녕"/>
+                <Card icon={<DiJsBadge size="125" color="#FF9614"/>} skill="JavaScript" text="안녕"/>
             </Wrapper>
         </SkillsBlock>
     );
