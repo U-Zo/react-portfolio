19bb3ec0a3e1c3141a6b3c7bdddc895cf509a36d
u_zo <kwoojo0127@naver.com>
2020-06-20 17:03:39 +0900

    Move reset.css to lib directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
+import './lib/reset.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
