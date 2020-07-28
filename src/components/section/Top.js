25316aa112c9c0f1c33dd4335a183d192c75bea6
Woojo <kwoojo0127@naver.com>
2020-07-28 14:37:00 +0900

    Update temporary main screen

--- src/components/section/Top.js
+++ src/components/section/Top.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import backImg from '../../lib/img/temp.jpg';
 
 const TopBlock = styled.div`
-  height: 750px;
-  background: #555;
+  height: 700px;
+  background: url(${backImg}) no-repeat center center;
+  background-size: contain;
 `;
 
 const Top = () => {
