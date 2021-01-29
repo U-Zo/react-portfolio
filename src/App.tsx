0ca35dab3ee2e430a9cf583f4ea5931f9032b1e4
Woojo Kim <kwoojo0127@gmail.com>
2021-01-29 13:02:35 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HeaderContainer from './containers/HeaderContainer';
 
 const App = () => {
   return (
     <>
-      <div></div>
+      <HeaderContainer />
     </>
   );
 };
